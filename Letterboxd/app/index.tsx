c5bc8967967047411c11a8ac1833fef44b6c7b2e
David Melquíades <davidmelki08@gmail.com>
2026-03-10 10:24:44 -0300

    atualizar trabalho expo

--- Letterboxd/app/index.tsx
+++ Letterboxd/app/index.tsx
@@ -1,15 +1,36 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import {Feather} from "@expo/vector-icons";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View style={styles.container }
     >
+      <View style={styles.barraSuperior}>
+      <Feather  name="settings" size={40} color="#ffff"/>
+
+      </View>
+
+
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles= StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"#181B20",
+  },
+
+  barraSuperior:{
+    backgroundColor:"#000000",
+    height:85,
+    justifyContent:"center",
+    paddingHorizontal:10,
+  },
+
+
+
+
+
+});
